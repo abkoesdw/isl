648f097030dc63df59ad6e6a320ce923de348439
Arief Koesdwiady <ariefbarkah@gmail.com>
2023-07-10 16:48:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Intro
+
+This is my attempt to reproduce examples presented in the _Introduction to Statistical Learning_<sup>1</sup> book
+
+## Reference
+
+1. James, Gareth, et al. An introduction to statistical learning. Vol. 112. New York: springer, 2013
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.0.0"
+PACKAGE_NAME = "ISL"
+MAINTAINER = "abkoesdw"
+MAINTAINER_EMAIL = "ariefbarkah@gmail.com"
+
+DESCRIPTION = "This is my repo to reproduce examples in ISL"
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name=PACKAGE_NAME,
+    version=__version__,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==2.0.3",
+    ],
+    extras_require={"dev": ["black==23.3.0"]},
+    license="",
+    python_requires=">=3.8",
+)
